276addcbf591a4ac211236371fa65a4b6307a3d6
Truetirtha <patitirtharaj@gmail.com>
2025-09-10 18:18:05 +0530

    chore: Add placeholder PHPUnit test for future use

---
+++ tests/DummyTest.php
@@ -0,0 +1,12 @@
+<?php
+// File: tests/DummyTest.php
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function testPlaceholder()
+    {
+        $this->assertTrue(true);
+    }
+}
