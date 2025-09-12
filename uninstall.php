a27dddc4ddeb1a37880d8337f973fafd02ee1f49
Truetirtha <patitirtharaj@gmail.com>
2025-09-12 11:57:44 +0530

    feat: Add proper cleanup in uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -2,4 +2,23 @@
 
 defined('WP_UNINSTALL_PLUGIN') || exit;
 
-// silence is golden
+// Remove widget instances
+delete_option('widget_latest-post-widget');
+
+// Cleanup sidebar assignments
+$sidebars = get_option('sidebar_widgets');
+if (is_array($sidebars)) {
+    foreach ($sidebars as $sidebar => $widgets) {
+        if (is_array($widget)) {
+            $sidebars[$sidebar] = array_filter($widgets, function ($widget) {
+                return strpos($widget, 'latest-post-widget') != 0;
+            });
+        }
+    }
+    update_option($sidebars);
+}
+// Clear any transients plugin might have created
+delete_transient('latest_post_widget_errors');
+
+// Clear object name
+wp_cache_flush();
